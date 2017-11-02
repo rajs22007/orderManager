0844090e85c9aee79a7267e275c4842b4f998528
Ashish Raj <Ashish.Raj@altisource.com>
2017-11-02 23:23:43 +0530

    pull bpmn from resource using classloader

--- src/main/java/com/sr/om/service/impl/OrderServiceImpl.java
+++ src/main/java/com/sr/om/service/impl/OrderServiceImpl.java
@@ -1,6 +1,5 @@
 package com.sr.om.service.impl;
 
-import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.util.HashMap;
 import java.util.List;
@@ -13,60 +12,71 @@ import org.activiti.engine.ProcessEngineConfiguration;
 import org.activiti.engine.RepositoryService;
 import org.activiti.engine.RuntimeService;
 import org.activiti.engine.TaskService;
-import org.activiti.engine.repository.Deployment;
-import org.activiti.engine.repository.DeploymentBuilder;
 import org.activiti.engine.runtime.ProcessInstance;
 import org.activiti.engine.task.Task;
 import org.activiti.engine.task.TaskQuery;
 import org.apache.commons.lang3.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 import com.sr.om.dal.OrderRepository;
 import com.sr.om.dal.mysql.model.Order;
 import com.sr.om.service.OrderService;
 
-@Service(value="orderService")
+@Service(value = "orderService")
 public class OrderServiceImpl implements OrderService {
 
 	private static final String CREATED_STATE = "Created";
-	
+
+	private static final Logger LOGGER = LoggerFactory.getLogger(OrderServiceImpl.class);
+
 	@Inject
 	private OrderRepository orderRepository;
-	
+
 	@Inject
 	OrderUpdateDelegate orderUpdateDelegate;
-	
-	ProcessEngineConfiguration processEngineConfiguration  = ProcessEngineConfiguration.createStandaloneProcessEngineConfiguration();
-	
-	ProcessEngine processEngine = processEngineConfiguration.setDatabaseSchemaUpdate(ProcessEngineConfiguration.DB_SCHEMA_UPDATE_TRUE).setJdbcUrl("jdbc:mysql://127.0.0.1:3306/sr_data?characterEncoding=UTF-8").setJdbcDriver("com.mysql.jdbc.Driver").setJdbcUsername("root").setJdbcPassword("root").buildProcessEngine();
-	
-	//@Autowired
-    private RepositoryService repositoryService = processEngine.getRepositoryService();
 
-//	@Inject
+	ProcessEngineConfiguration processEngineConfiguration = ProcessEngineConfiguration
+			.createStandaloneProcessEngineConfiguration();
+
+	ProcessEngine processEngine = processEngineConfiguration
+			.setDatabaseSchemaUpdate(ProcessEngineConfiguration.DB_SCHEMA_UPDATE_TRUE)
+			.setJdbcUrl("jdbc:mysql://127.0.0.1:3306/sr_data?characterEncoding=UTF-8")
+			.setJdbcDriver("com.mysql.jdbc.Driver").setJdbcUsername("root").setJdbcPassword("root")
+			.buildProcessEngine();
+
+	// @Autowired
+	private RepositoryService repositoryService = processEngine.getRepositoryService();
+
+	// @Inject
 	private RuntimeService runtimeService = processEngine.getRuntimeService();
-	
+
 	private TaskService taskService = processEngine.getTaskService();
-	
-	
 
-	/*private OrderRepository orderEntityRepository;
-	private RuntimeService runtimeService;*/
+	{
+		repositoryService.createDeployment().addInputStream("NewOrderProcess.bpmn",
+				getClass().getClassLoader().getResourceAsStream("processes/NewOrderProcess.bpmn")).deploy();
+	}
 
-	/*public OrderServiceImpl(final OrderRepository entityRepository, final RuntimeService runtimeService) {
-		this.orderEntityRepository = checkNotNull(entityRepository);
-		this.runtimeService = checkNotNull(runtimeService);
-	}*/
+	/*
+	 * private OrderRepository orderEntityRepository; private RuntimeService
+	 * runtimeService;
+	 */
+
+	/*
+	 * public OrderServiceImpl(final OrderRepository entityRepository, final
+	 * RuntimeService runtimeService) { this.orderEntityRepository =
+	 * checkNotNull(entityRepository); this.runtimeService =
+	 * checkNotNull(runtimeService); }
+	 */
 
 	public Map<String, String> createOrder(Order orderEntity) throws FileNotFoundException {
-		DeploymentBuilder deploymentBuilder  = repositoryService.createDeployment().addInputStream("NewOrderProcess.bpmn", new FileInputStream("D:/Projects/iAppraisal/sample-master/orderManager-master/src/main/resources/processes/NewOrderProcess.bpmn"));
-		Deployment deployment = deploymentBuilder.deploy();
-		System.out.println(deployment.getName());
-		System.out.println("Number of process definitions: " + repositoryService.createProcessDefinitionQuery().count());
-		System.out.println("Create OrderEntity request: " + orderEntity.toString());
+		LOGGER.info("Number of process definitions: ", repositoryService.createProcessDefinitionQuery().count());
+		LOGGER.info("Create OrderEntity request: ", orderEntity.toString());
 		orderEntity.setStatuCode(CREATED_STATE);
 		Order savedOrderEntity = orderRepository.insertAndFlushReturnObj(orderEntity);
-		System.out.println(savedOrderEntity.toString());
+		LOGGER.info(savedOrderEntity.toString());
 		if (StringUtils.isNotEmpty(orderEntity.getVendorUid()))
 			return startProcess(savedOrderEntity.getId(), orderEntity.getVendorUid());
 
@@ -100,12 +110,10 @@ public class OrderServiceImpl implements OrderService {
 		vars.put("orderId", orderId);
 		vars.put("orderUpdateDelegate", orderUpdateDelegate);
 		ProcessInstance instance = runtimeService.startProcessInstanceByKey("NewOrderProcess", vars);
-		//instance.getProcessVariables().put("orderId", orderId);
-		//instance.getProcessVariables().put("orderEntity", orderEntity);
 
-		System.out.println("Process started...");
-		System.out.println("Process details: " + instance.toString());
-		System.out.println("Process Variables: " + instance.getProcessVariables().toString());
+		LOGGER.info("Process started...");
+		LOGGER.info("Process details: ", instance.toString());
+		LOGGER.info("Process Variables: ", instance.getProcessVariables().toString());
 
 		if (StringUtils.isNotEmpty(vendorUid)) {
 			orderEntity.setVendorUid(vendorUid);
@@ -135,39 +143,42 @@ public class OrderServiceImpl implements OrderService {
 	}
 
 	public void updateVendorDetails() {
-		System.out.println("In porgress...");
+		LOGGER.info("In porgress...");
 	}
 
 	public void orderCompletedGreetings() {
-		System.out.println("Order has been succufully completed...");
+		LOGGER.info("Order has been succufully completed...");
 	}
 
 	@Override
 	public String getTasksById(Long orderId) {
 		List<Task> tasks = fetchTaskQuery().processVariableValueEquals(orderId).orderByDueDateNullsFirst().asc().list();
-		
-		for(Task task: tasks){
-			System.out.println("taskId: " + task.getId() + ", taskName: " + task.getName() + ", assignee: " + task.getAssignee() + "task variables: " + task.getProcessVariables());
+
+		for (Task task : tasks) {
+			LOGGER.info("taskId: {}, taskName: {}, assignee: {}, task variables: {}", task.getId(), task.getName(),
+					task.getAssignee(), task.getProcessVariables());
 			return task.toString();
 		}
-		
+
 		return tasks.toString();
 	}
 
 	@Override
 	public String getTasks() {
 		List<Task> tasks = fetchTaskQuery().orderByDueDateNullsFirst().asc().list();
-		for(Task task: tasks){
-			System.out.println("taskId: " + task.getId() + ", taskName: " + task.getName() + ", assignee: " + task.getProcessVariables() + task.getTaskLocalVariables());
+		for (Task task : tasks) {
+			LOGGER.info("taskId: {}, taskName: {}, assignee: {}, task variables: {}", task.getId(), task.getName(),
+					task.getAssignee(), task.getProcessVariables());
 		}
 		return tasks.toString();
 	}
-	
+
 	@Override
 	public String getTasksByAssignee(String assignee) {
 		List<Task> tasks = fetchTaskQuery().taskAssignee(assignee).orderByDueDateNullsFirst().asc().list();
-		for(Task task: tasks){
-			System.out.println("taskId: " + task.getId() + ", taskName: " + task.getName() + ", assignee: " + task.getProcessVariables() + task.getTaskLocalVariables());
+		for (Task task : tasks) {
+			LOGGER.info("taskId: {}, taskName: {}, assignee: {}, task variables: {}", task.getId(), task.getName(),
+					task.getAssignee(), task.getProcessVariables());
 		}
 		return tasks.toString();
 	}
@@ -175,8 +186,9 @@ public class OrderServiceImpl implements OrderService {
 	@Override
 	public String getTasksWithNoAssignee() {
 		List<Task> tasks = fetchTaskQuery().taskUnassigned().orderByDueDateNullsFirst().asc().list();
-		for(Task task: tasks){
-			System.out.println("taskId: " + task.getId() + ", taskName: " + task.getName() + ", assignee: " + task.getProcessVariables() + task.getTaskLocalVariables());
+		for (Task task : tasks) {
+			LOGGER.info("taskId: {}, taskName: {}, assignee: {}, task variables: {}", task.getId(), task.getName(),
+					task.getAssignee(), task.getProcessVariables());
 		}
 		return tasks.toString();
 	}
